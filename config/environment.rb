c57eebba2f4bfcdb19bd6f7129c1b55c9ae0ee9f
Ian Okumu <okumu.otsembo@gmail.com>
2022-08-24 06:24:00 +0300

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+require_relative '../lib/student'
+
+DB = { conn: SQLite3::Database.new("db/school.db") }
+
+# RUN CODE FROM HERE
---
+++ lib/student.rb
@@ -0,0 +1,24 @@
+class Student
+    attr_accessor :name, :age, :id
+
+    def initialize(name:, age:, id: nil)
+        @id=id
+        @name=name
+        @id=id
+    end
+
+# TODO: CREATE TABLE
+
+# TODO: INSERT RECORD
+
+# TODO: SHOW ALL RECORDS
+
+# TODO: UPDATE RECORD
+
+# TODO: DELETE RECORD
+
+# TODO: CONVERT TABLE RECORD TO RUBY OBJECT
+
+# TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS
+
+end
